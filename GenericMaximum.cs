813432ddecad3d9a2b9d9d502a1fa92498e7b2c9
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-01-17 16:42:12 +0530

    Add files via upload

---
+++ GenericMaximum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericMaximum(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValues(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[sorted_values.Length-1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValues(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValues(this.value);
+            Console.WriteLine("maximum value is " +max);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using FindMaximumNumber;
+
+Console.WriteLine("Find the Maximum Number : ");
+int output = MaximumNumberCheck.MaximumNumber(25, 15, 13);
+Console.WriteLine(output);
+double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(52.26, 18.92, 15.12);
+Console.WriteLine(doubleoutput);
+string strigoutput = MaximumNumberCheck.MaximumStringNumber("linklist", "shellscript", "visualstudio");
+Console.WriteLine(strigoutput);
+int[] intArray = { 102, 152, 165, 205, 209, 215 };
+GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+generic.PrintMaxValue();
+double[] doubleArray = { 2.63, 25.36, 52.09, 12.32, 46.15 };
+GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+genericDouble.PrintMaxValue();
+string[] stringArray = { "araqu", "meedaram", "beach", "agra", "ooti" };
+GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+genericString.PrintMaxValue();
+Console.ReadKey();
